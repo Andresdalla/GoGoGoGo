1f3785a9b0f48841fe5b4fd008db5e4fec4ec440
Andres Dalla Rizza <andresdallarizza1081@gmail.com>
2024-07-15 20:06:26 -0300

    first commit

---
+++ hello.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+//Traigo el package quote de https://pkg.go.dev/
+//Una vez que hago el import, tengo que hacer go mod tidy
+// para que lo localice y lo traiga 
+import "rsc.io/quote"
+
+func main() {
+	fmt.Println(quote.Go())
+}
